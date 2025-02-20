0900c07e9768fa46ac14bb88b7e756a0c298b00d
Infstellar <2644641825@qq.com>
2025-02-20 20:43:46 +0800

    feat: add ui with label memes

---
+++ pages/label_images.py
@@ -0,0 +1,132 @@
+import os
+import shutil
+import time
+
+import numpy as np
+import streamlit as st
+import random
+import yaml
+from PIL import Image
+
+from services.image_search import ImageSearch
+from config.settings import config, reload_config
+from pages.utils import *
+from services.label_memes import LabelMemes
+
+
+
+st.set_page_config(
+    page_title="LabelImages",
+    page_icon="🌐",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+if 'image_folder_name' not in st.session_state:
+    st.session_state.image_folder_name = 'Please Select a Folder'
+if 'image_index' not in st.session_state:
+    st.session_state.image_index = 0
+if 'label_meme_obj' not in st.session_state:
+    st.session_state.label_meme_obj = LabelMemes()
+if 'new_file_name' not in st.session_state:
+    st.session_state.new_file_name = ''
+if 'can_add_vlm_result_to_filename' not in st.session_state:
+    st.session_state.can_add_vlm_result_to_filename = False
+if 'auto_generate_labels' not in st.session_state:
+    st.session_state.auto_generate_labels = False
+if 'result_folder_name' not in st.session_state:
+    st.session_state.result_folder_name = ''
+    if 'image_folder_name' in st.session_state:
+        st.session_state.result_folder_name = st.session_state.image_folder_name
+
+def onchange_folder_name():
+    st.session_state.image_index = 0
+    st.session_state.all_images_path = get_all_file_paths(st.session_state.image_folder_name)
+
+
+
+with st.sidebar:
+    st.text_input('原图文件夹', on_change=onchange_folder_name, key='image_folder_name')
+    st.text_input('生成结果文件夹', on_change=onchange_folder_name, key='result_folder_name')
+
+
+
+
+if os.path.exists(st.session_state.image_folder_name):
+
+    st.write(st.session_state.image_folder_name)
+    img_path = st.session_state.all_images_path[st.session_state.image_index]
+    st.write(img_path)
+    with Image.open(img_path) as img:
+        img_obj = img.copy()
+    st.image(img_obj)
+
+    col3, col4, col5 = st.columns([1, 1, 1])
+
+    def onclick_use_vlm_generate():
+        try:
+            img_path = st.session_state.all_images_path[st.session_state.image_index]
+            st.session_state.img_analyse_result = st.session_state.label_meme_obj.label_image(img_path)
+            st.session_state.can_add_vlm_result_to_filename = True
+            return True
+        except Exception as e:
+            st.error(f"VLM 生成描述失败: {str(e)}")
+            return False
+    with col3:
+        st.button('使用VLM生成描述', on_click = onclick_use_vlm_generate)
+
+    def get_labals_with_vlm():
+        try:
+            name_list = st.session_state.img_analyse_result
+            colB1, colB2, colB3 = st.columns([1, 1, 1])
+            for index, i in enumerate([colB1, colB2, colB3]):
+                with i:
+                    def create_onc(inner_index):
+                        def onc():
+                            st.session_state.new_file_name += name_list[inner_index]
+                        return onc
+
+                    st.button(f"添加 \"{name_list[index]}\" 到文件名", on_click=create_onc(index),key=f'generate_clicked_{index}')
+
+                    # auto mode
+                    if st.session_state.auto_generate_labels:
+                        if index in [0,1]:
+                            st.session_state.new_file_name += name_list[index]
+        except Exception as e:
+            st.error(f"VLM 生成描述失败: {str(e)}")
+
+
+    st.text_input('New file name', key='new_file_name')
+
+    if st.session_state.can_add_vlm_result_to_filename:
+        img_path = st.session_state.all_images_path[st.session_state.image_index]
+        get_labals_with_vlm()
+
+
+    def onclick_rename_file():
+        original_path = st.session_state.all_images_path[st.session_state.image_index]
+        new_path = os.path.join(st.session_state.result_folder_name, st.session_state.new_file_name+os.path.splitext(os.path.basename(original_path))[1])
+        try:
+            os.rename(original_path, new_path)
+        except Exception as e:
+            st.error(f"重命名文件失败: {str(e)}")
+            return False
+        st.session_state.all_images_path[st.session_state.all_images_path.index(original_path)] = new_path
+        return True
+    st.button('重命名文件', on_click=onclick_rename_file)
+
+    col1, _, col2 = st.columns([1,2,1])
+    def onc1():
+        st.session_state.image_index -= 1
+        st.session_state.can_add_vlm_result_to_filename = False
+        st.session_state.new_file_name = ''
+    def onc2():
+        st.session_state.image_index += 1
+        st.session_state.can_add_vlm_result_to_filename = False
+        st.session_state.new_file_name = ''
+    with col1:
+        st.button('Previous', on_click=onc1)
+
+    with col2:
+        st.button('Next', on_click=onc2)
+
--- services/label_memes.py
+++ services/label_memes.py
@@ -7,7 +7,7 @@ import cv2
 PROMOTE = """你是一位表情包分类专家。请分析这个表情包，要求：
 
 1. 简要分析表情包的内容，含义，主体和可能的使用场景；如果表情包有文字，你应该仔细分析考虑，因为表情包的文字通常有幽默感而难以理解；
-2. 按格式要求输出表情包的文本描述，格式如下： 表情包含义:(一句话概括表情包；如果表情包有文字，你要写出); 表情包主体:(提取表情包的主角); 表情包使用场景:(一句话描述表情包可能的使用场景)
+2. 按格式要求输出表情包的文本描述，格式如下： **表情包含义**:(几个关键词概括表情包；如果表情包有文字，你要写出); **表情包主体**:(提取表情包的主角); **表情包使用场景**:(几个关键词描述表情包可能的使用场景)
 
 
 """
@@ -18,6 +18,7 @@ class LabelMemes():
         self.endpoint = "https://api.siliconflow.com/v1/embeddings"
         self.cache = {}
         self._load_cache()
+        self.use_cache = False
 
     def _load_cache(self):
         cache_file = config.get_label_images_cache_file()
@@ -31,28 +32,45 @@ class LabelMemes():
         with open(cache_file, 'wb') as f:
             pickle.dump(self.cache, f)
 
+    def _analyze_result_text(self, text:str):
+        if not '**表情包含义**' in text or not '**表情包主体**' in text or not '**表情包使用场景**' in text:
+            raise Exception(f'analyze result text error: {text}; 模型太蠢,换个模型或者重试')
+        desc = text.split('**表情包含义**')[-1]
+        character = desc.split('**表情包主体**')[-1]
+        usage = character.split('**表情包使用场景**')[-1]
+        def clean_some_characters(x, l):
+            for i in l:
+                x = x.replace(i, '')
+            return x
+        desc = desc.replace(character, '')
+        character = character.replace(usage, '')
+        laji = ['表情包主体', '表情包使用场景', ':', '**(', ')；**', ');**', '**', ');', ')', '；', '(', ')', '\n', '：']
+        desc = clean_some_characters(desc, laji).replace('/', ' ').replace('\\', ' ')
+        character = clean_some_characters(character, laji).replace('/', ' ').replace('\\', ' ')
+        usage = clean_some_characters(usage, laji).replace('/', ' ').replace('\\', ' ')
+        return desc, character, usage
+
     def label_image(self, image_path):
         # 检查缓存
         model_name = config.models.vlm_models['Qwen2-VL-72B-Instruct'].name
         if not model_name in self.cache.keys():
             self.cache[model_name] = {}
-        if get_file_hash(image_path) in self.cache[model_name]:
-            return self.cache[model_name][get_file_hash(image_path)]['description']
-
+        if get_file_hash(image_path) in self.cache[model_name] and self.use_cache:
+            return self._analyze_result_text(self.cache[model_name][get_file_hash(image_path)]['description'])
 
         # 以二进制模式读取图片
-        with open(image_path, 'rb') as f:
-            img_data = f.read()
-
-        # 将读取的数据转换为numpy数组
-        img_array = np.frombuffer(img_data, np.uint8)
-
-        # 解码数组得到图像
-        image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
-        # 将图像编码为 JPEG 格式
-        _, img_encoded = cv2.imencode(".png", image)
-        img_str = base64.b64encode(img_encoded).decode("utf-8")
+        # with open(image_path, 'rb') as f:
+        #     img_data = f.read()
+        #
+        # # 将读取的数据转换为numpy数组
+        # img_array = np.frombuffer(img_data, np.uint8)
+        #
+        # # 解码数组得到图像
+        # image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        #
+        # # 将图像编码为 JPEG 格式
+        # _, img_encoded = cv2.imencode(".png", image)
+        img_str = image_to_base64_jpg(image_path)#.decode("utf-8")
 
         import requests
 
@@ -61,7 +79,7 @@ class LabelMemes():
         url = "https://api.siliconflow.cn/v1/chat/completions"
 
         payload = {
-            "model": config.models.vlm_models['Qwen2-VL-72B-Instruct'].name,
+            "model": config.models.vlm_models['Qwen2-VL-72B-Instruct'].name, # Qwen2-VL-72B-Instruct
             "messages": [
                 {
                     "role": "system",
@@ -120,12 +138,13 @@ class LabelMemes():
             response = requests.request("POST", url, json=payload, headers=headers)
             response.raise_for_status()  # 抛出详细的HTTP错误
             description = response.json()['choices'][0]['message']['content']
+            self._analyze_result_text(description)
         except requests.exceptions.RequestException as e:
             if hasattr(e.response, 'status_code') and e.response.status_code == 400:
                 # 尝试打印详细的错误信息
                 error_msg = e.response.json() if e.response.text else "未知错误"
-                print(f"API请求参数错误: {error_msg}")
-            raise RuntimeError(f"API请求失败: {str(e)}\n请求参数: {payload}")
+                print(f"API请求参数错误: {str(error_msg).replace(img_str, 'IMGDATA')}")
+            raise RuntimeError(f"API请求失败: {str(e)}\n请求参数: {str(payload).replace(img_str, 'IMGDATA')}")
 
         # 缓存结果
         self.cache[model_name][get_file_hash(image_path)] = {
@@ -134,7 +153,7 @@ class LabelMemes():
         }
         self._save_cache()
 
-        return description
+        return self._analyze_result_text(description)
 
 if __name__ == "__main__":
     lm = LabelMemes()
