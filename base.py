14ea40b787126176e92a9b5c7da797960a1d0c5d
Infstellar <2644641825@qq.com>
2025-03-02 22:35:53 +0800

    some fixes in #42

--- base.py
+++ base.py
@@ -1,6 +1,8 @@
 import os
+import re
 import sys
 from loguru import logger
+import typing as t
 def verify_folder(root):
     if '.' in os.path.basename(root):
         root = os.path.dirname(root)
@@ -13,3 +15,9 @@ def verify_folder(root):
 
 TRACE_MODE = os.path.exists("trace") or os.path.exists("trace.txt") # 最高等级日志输出
 DEBUG_MODE = sys.gettrace() is not None or TRACE_MODE # 调试模式
+
+def remove_invalid_filename_chars(filename):
+    # 定义正则表达式，匹配Windows文件名中不支持的字符
+    pattern = r'[\\/*?:"<>|]'
+    # 使用空字符串替换匹配到的字符
+    return re.sub(pattern, ' ', filename)
--- pages/upload_images.py
+++ pages/upload_images.py
@@ -34,6 +34,8 @@ if 'base_url' not in st.session_state:
     st.session_state.base_url = Config().api.vlm_models.base_url
     if st.session_state.base_url is None:
         st.session_state.base_url = ''
+if 'upload_file_key_2' not in st.session_state:
+    st.session_state.upload_file_key_2 = int(time.time()*100)
 
 
 def on_api_key_change():
@@ -103,7 +105,7 @@ with st.sidebar:
         key="base_url_input",
         on_change=on_base_url_change
     )
-    uploaded_images = st.file_uploader(label='添加表情包', accept_multiple_files=True, type=['png', 'jpg', 'jpeg', 'gif'])
+    uploaded_images = st.file_uploader(label='添加表情包', accept_multiple_files=True, type=['png', 'jpg', 'jpeg', 'gif'], key=st.session_state.upload_file_key_2)
 
 
 
@@ -144,15 +146,14 @@ if uploaded_images:
             img = Image.open(uploaded_image)
             show_image_area.image(img, caption=uploaded_image.name, width=200)
             cache_path = os.path.join(CACHE_PATH, img_name)
-            if img.mode == 'P': #TODO: 检查这行代码是否不需要
-                img = img.convert('RGB')
+            img = img.convert('RGB')
             img.save(os.path.join(cache_path))
             # 生成图片描述
             if st.session_state.auto_generate_labels:
                 res = label_image(cache_path, show_result_area)
                 if res:
                     img_name = res + os.path.splitext(img_name)[1]
-
+            img_name = remove_invalid_filename_chars(img_name)
             # 保存上传的图片到指定文件夹
             save_image_path = os.path.join(st.session_state.image_folder_name,img_name )
             if os.path.exists(save_image_path):
@@ -165,4 +166,4 @@ if uploaded_images:
         except Exception as e:
             logger.exception(e)
             show_result_area.error(f'图片 {uploaded_image.name} 保存失败: {e}')
-    uploaded_images = [] # 这行代码没用，那个组件仍然会保存已上传的文件
+    st.session_state.upload_file_key_2 = int(time.time()*100)
